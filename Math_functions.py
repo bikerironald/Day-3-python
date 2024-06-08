97a5f67a73a06229302338af3ae6b612ec2d2cbb
Ronarld Bikeri <bikerironald@gmail.com>
2024-06-08 03:09:01 +0300

    Add files via upload
    Math functions
    user input

---
+++ Math_functions.py
@@ -0,0 +1,26 @@
+# math functios
+"""abs(x)	Returns the absolute value of x
+acos(x)	Returns the arccosine of x
+asin(x)	Returns the arcsine of x
+atan(x)	Returns the arctangent of x
+cbrt(x)	Returns the cube root of x
+cos(x)	Returns the cosine of x
+exp(x)	Returns the value of Ex
+sin(x)	Returns the sine of x (x is in radians)
+tan(x)	Returns the tangent of an angle
+import math"""
+
+# print(math.pi)
+# print(math.e)
+# print(math.inf)
+# print(math.ceil(2.4))
+# print(math.floor(2.4))
+# print(math.factorial(5))
+# print(math.gcd(10, 15))
+
+
+import math
+
+pi = 30.0
+# print(round(pi))
+print(math.floor(pi))
---
+++ user_input.py
@@ -0,0 +1,18 @@
+name = input('what is your first name?: ')
+age = input('What is your age?: ')
+height = input('Enter your height: ')
+religion = input('Enter your religion: ')
+
+print(name)
+
+print('You are '+ age + ' years old')
+
+print('you are ' + height)
+
+print('Your religion is '+ religion)
+
+
+
+
+
+# print(f'Hello {f_name} {s_name} you are {age} years old {height} fr tall and your religion is {religion}')
